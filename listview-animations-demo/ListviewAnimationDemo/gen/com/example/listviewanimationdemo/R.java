20829be8055c1f47dd93258de1cb7b52ff306a89
waylife <waylife365@gmail.com>
2014-07-06 02:26:03 +0800

    1.item animation,add 2.other animation,add

--- listview-animations-demo/ListviewAnimationDemo/gen/com/example/listviewanimationdemo/R.java
+++ listview-animations-demo/ListviewAnimationDemo/gen/com/example/listviewanimationdemo/R.java
@@ -25,36 +25,55 @@ public final class R {
         public static final int activity_animateremoval_listview=0x7f070001;
         public static final int activity_baselist_listview=0x7f070002;
         public static final int activity_draganddrop_listview=0x7f070003;
-        public static final int activity_expandablelistitem_card_content=0x7f07000c;
-        public static final int activity_expandablelistitem_card_title=0x7f07000b;
-        public static final int home_item_animation_btn=0x7f070004;
-        public static final int home_item_drag_btn=0x7f070006;
-        public static final int home_item_expend_btn=0x7f070008;
-        public static final int home_item_go_github=0x7f070009;
-        public static final int home_item_go_la_github=0x7f07000a;
-        public static final int home_multiple_item_dismiss_btn=0x7f070007;
-        public static final int home_swipe_to_dismiss_btn=0x7f070005;
-        public static final int menu_expandable_limit=0x7f07000f;
-        public static final int swipe_menu_main=0x7f070010;
-        public static final int swipe_menu_with_undo=0x7f070011;
-        public static final int swipe_menu_with_undo_2=0x7f070012;
-        public static final int swipe_menu_with_undo_3=0x7f070013;
-        public static final int undo_row_texttv=0x7f07000d;
-        public static final int undo_row_undobutton=0x7f07000e;
+        public static final int activity_expandablelistitem_card_content=0x7f070014;
+        public static final int activity_expandablelistitem_card_title=0x7f070013;
+        public static final int activity_googlecards_card_imageview=0x7f070006;
+        public static final int activity_googlecards_card_textview=0x7f070005;
+        public static final int activity_googlecards_listview=0x7f070004;
+        public static final int activity_gridview_gv=0x7f070007;
+        public static final int home_item_add_btn=0x7f07000d;
+        public static final int home_item_animation_btn=0x7f070008;
+        public static final int home_item_drag_btn=0x7f07000a;
+        public static final int home_item_expend_btn=0x7f07000c;
+        public static final int home_item_go_github=0x7f07000e;
+        public static final int home_item_go_la_github=0x7f07000f;
+        public static final int home_multiple_item_dismiss_btn=0x7f07000b;
+        public static final int home_swipe_to_dismiss_btn=0x7f070009;
+        public static final int item_animation_main_1=0x7f070010;
+        public static final int item_animation_main_google_card=0x7f070011;
+        public static final int item_animation_main_gridview=0x7f070012;
+        public static final int menu_expandable_limit=0x7f070017;
+        public static final int menu_item_animation_alpha=0x7f070018;
+        public static final int menu_item_animation_bottom=0x7f07001b;
+        public static final int menu_item_animation_bottom_right=0x7f07001c;
+        public static final int menu_item_animation_left=0x7f070019;
+        public static final int menu_item_animation_right=0x7f07001a;
+        public static final int menu_item_animation_scale=0x7f07001d;
+        public static final int swipe_menu_main=0x7f07001e;
+        public static final int swipe_menu_with_undo=0x7f07001f;
+        public static final int swipe_menu_with_undo_2=0x7f070020;
+        public static final int swipe_menu_with_undo_3=0x7f070021;
+        public static final int undo_row_texttv=0x7f070015;
+        public static final int undo_row_undobutton=0x7f070016;
     }
     public static final class layout {
         public static final int activity_animateremoval=0x7f030000;
         public static final int activity_baselist=0x7f030001;
         public static final int activity_draganddrop=0x7f030002;
-        public static final int activity_home=0x7f030003;
-        public static final int item_animateremoval_row=0x7f030004;
-        public static final int item_expandablelistitem_card=0x7f030005;
-        public static final int item_list_row=0x7f030006;
-        public static final int item_undo_row=0x7f030007;
+        public static final int activity_googlecards=0x7f030003;
+        public static final int activity_googlecards_card=0x7f030004;
+        public static final int activity_gridview=0x7f030005;
+        public static final int activity_home=0x7f030006;
+        public static final int activity_item_animation_main=0x7f030007;
+        public static final int item_animateremoval_row=0x7f030008;
+        public static final int item_expandablelistitem_card=0x7f030009;
+        public static final int item_list_row=0x7f03000a;
+        public static final int item_undo_row=0x7f03000b;
     }
     public static final class menu {
         public static final int menu_expandablelistitem=0x7f060000;
-        public static final int swipe_to_dismiss=0x7f060001;
+        public static final int menu_item_animation=0x7f060001;
+        public static final int menu_swipe_to_dismiss=0x7f060002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
