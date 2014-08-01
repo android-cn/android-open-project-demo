c3b22228f6753ab0884a19a11f2f33e62be02162
zmywly8866 <zmywly8866@163.com>
2014-08-01 08:14:54 +0800

    提交fancycoverflow-demo初版
    提交fancycoverflow-demo初版

---
+++ fancycoverflow-demo/src/com/mingy/fancycoverflow/demo/FancyCoverFlowMainActivity.java
@@ -0,0 +1,13 @@
+package com.mingy.fancycoverflow.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FancyCoverFlowMainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_fancycoverflow_layout);
+	}
+}
