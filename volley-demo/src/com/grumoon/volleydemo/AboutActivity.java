21d527b1109a881c697e47e5de0bf8bb709febb6
grumoon <zhangxzfly@163.com>
2014-11-21 15:55:35 +0800

    add about

--- volley-demo/src/com/grumoon/volleydemo/AboutActivity.java
+++ volley-demo/src/com/grumoon/volleydemo/AboutActivity.java
@@ -2,12 +2,20 @@ package com.grumoon.volleydemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class AboutActivity extends Activity {
+
+	private TextView tvAbout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.app_about);
+		tvAbout = (TextView) findViewById(R.id.tv_about);
+		tvAbout.setMovementMethod(LinkMovementMethod.getInstance());
+		tvAbout.setText(Html.fromHtml((getResources().getString((R.string.about_text)))));
 	}
 }
--- volley-demo/src/com/grumoon/volleydemo/SampleActivity.java
+++ volley-demo/src/com/grumoon/volleydemo/SampleActivity.java
@@ -2,8 +2,6 @@ package com.grumoon.volleydemo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class SampleActivity extends Activity {
 
