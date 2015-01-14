c60d5206fab4a282455f2545046a410a0e5ce933
grumoon <zhangxzfly@163.com>
2015-01-14 11:37:58 +0800

    add TextView and WebView

--- android-Ultra-Pull-To-Refresh-demo/app/src/main/java/com/grumoon/androidultrapulltorefreshdemo/ui/ContentTextViewFragment.java
+++ android-Ultra-Pull-To-Refresh-demo/app/src/main/java/com/grumoon/androidultrapulltorefreshdemo/ui/ContentTextViewFragment.java
@@ -2,17 +2,32 @@ package com.grumoon.androidultrapulltorefreshdemo.ui;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.grumoon.androidultrapulltorefreshdemo.R;
+
+import org.w3c.dom.Text;
+
+import in.srain.cube.views.ptr.PtrClassicFrameLayout;
+import in.srain.cube.views.ptr.PtrDefaultHandler;
+import in.srain.cube.views.ptr.PtrFrameLayout;
+import in.srain.cube.views.ptr.PtrHandler;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContentTextViewFragment extends Fragment {
+
+    private PtrClassicFrameLayout ptr;
+
+    private TextView tvAbout;
 
 
     @Override
@@ -25,6 +40,23 @@ public class ContentTextViewFragment extends Fragment {
 
     private void initView(View v) {
 
+        tvAbout=(TextView)v.findViewById(R.id.tv_about);
+        tvAbout.setMovementMethod(LinkMovementMethod.getInstance());
+        tvAbout.setText(Html.fromHtml((getResources().getString((R.string.app_about)))));
+
+        ptr = (PtrClassicFrameLayout) v.findViewById(R.id.ptr_main);
+
+        ptr.setPtrHandler(new PtrDefaultHandler() {
+            @Override
+            public void onRefreshBegin(PtrFrameLayout ptrFrameLayout) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ptr.refreshComplete();
+                    }
+                }, 2000);
+            }
+        });
     }
 
 
