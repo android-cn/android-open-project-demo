afecb5e20565425d31e00332aff0cee0e32fe872
demonyan <33101845@qq.com>
2015-11-11 09:15:28 +0800

    remove unused file

--- listview-animations-demo/ListviewAnimationDemo/app/src/main/java/com/example/listviewanimationdemo/itemmanipulation/ExpandableListItemActivity.java
+++ listview-animations-demo/ListviewAnimationDemo/app/src/main/java/com/example/listviewanimationdemo/itemmanipulation/ExpandableListItemActivity.java
@@ -26,6 +26,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
